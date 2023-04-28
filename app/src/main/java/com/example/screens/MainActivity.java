7d8a2cc7cb67e0be78fa01945cc702b5c8a71d61
E. IGuz <a22edvbu@student.his.se>
2023-04-28 11:39:57 +0200

    Användaren kan nu skriva in en text i "editText" som sedan kan läsas i "SecondActivity" via putExtra och getExtra.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +19,11 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                EditText editTxt = findViewById(R.id.editText);
 
+                Intent screenIntent = new Intent(MainActivity.this, SecondActivity.class);
+                screenIntent.putExtra("input", editTxt.getText().toString()); // Optional
+                startActivity(screenIntent);
             }
         });
     }
--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle dataInput = getIntent().getExtras();
+        if (dataInput != null) {
+            String data = dataInput.getString("input");
+            TextView output = findViewById(R.id.textView2);
+            output.setText(data);
+        }
     }
 }
